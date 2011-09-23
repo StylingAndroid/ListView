3a78c1810dbb85de679a65ac5209466cc9a74b55
Mark Allison <mark@stylingandroid.com>
2011-09-23 11:49:11 +0100

    Completed Part 1

--- ListView/src/com/stylingandroid/listview/ListViewActivity.java
+++ ListView/src/com/stylingandroid/listview/ListViewActivity.java
@@ -1,13 +1,37 @@
 package com.stylingandroid.listview;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class ListViewActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.SimpleAdapter;
+
+public class ListViewActivity extends ListActivity
+{
+	@Override
+	public void onCreate( Bundle savedInstanceState )
+	{
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.main );
+		
+		List<Map<String, String>> data = 
+				new ArrayList<Map<String,String>>();
+		for( int i = 1; i <= 10; i++ )
+		{
+			Map<String, String> item = 
+					new HashMap<String, String>();
+			item.put( "name", String.format( "Item %d", i ) );
+			data.add( item );
+		}
+		
+		String[] from = { "name" };
+		int[] to = { android.R.id.text1 };
+		
+		SimpleAdapter adapter = new SimpleAdapter( this, data,
+				android.R.layout.simple_list_item_1, from, to );
+		setListAdapter( adapter );
+	}
 }
